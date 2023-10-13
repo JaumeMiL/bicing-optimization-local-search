00eb00ac8fa1e3f50c14f047ed6d66c2198a9ece
Joan Bernaus <joan.bernaus@estudiantat.upc.edu>
2023-10-13 19:06:59 +0200

    19:07

--- estat_bicing.py
+++ estat_bicing.py
@@ -1,1 +1,29 @@
+import random
+from abia_bicing import *
+class Estat:
+    def __init__(self, num_estacions, num_bicis, semilla, num_furgos):
+        self.estacions = Estaciones(numestacions, num_bicis, semilla)
+        self.visitat = [False] * num_estacions
+        self.furgonetes = [Furgoneta(i) for i in range(1, num_furgos+1)]
+        self.costos = 0
+        self.beneficis = 0
+        
+    def moure_bicis(self,origen,arribada1,arribada2):
+        furgoneta = self.furgonetes[origen]
+        #hauriem de tenir alguna forma d'identificar les estacions
+        
+        
 
+
+class Furgoneta:
+    
+    def __init__(self,origen):
+        self.origen = origen
+        self.capacitat = 30
+        
+    def set_origen(self,origen1):
+        self.origen = origen1
+        
+    def get_origen(self):
+        return self.origen
+    
