bcf37c82c230aab50d740cbdbe2630c5b78ad261
Joan Bernaus <joan.bernaus@estudiantat.upc.edu>
2023-10-15 18:57:50 +0200

    15/10 18:57

--- estat_bicing.py
+++ estat_bicing.py
@@ -1,12 +1,12 @@
 import random
+from copy import deepcopy
 from abia_bicing import *
 class Estat:
     def __init__(self, num_estacions, num_bicis, semilla, num_furgos):
         self.estacions = Estaciones(num_estacions, num_bicis, semilla)
         self.visitat = [False] * num_estacions
         self.furgonetes = [Furgoneta(i) for i in range(1, num_furgos+1)]
-        self.costos = 0
-        self.beneficis = 0
+        self.guanyat = 0
         
     def calcular_distancia_entre_estacions(self, estacio1, estacio2):
         x1, y1 = estacio1.coordX, estacio1.coordY
@@ -14,42 +14,72 @@ class Estat:
         distancia = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
         return distancia
         
-    def moure_bicis(self,origen,arribada1,arribada2):
-        furgoneta = self.furgonetes[origen]
+def moure_bicis(self, origen, arribada1, arribada2=None):
+    furgoneta = self.furgonetes[origen]
+    
+    # Funció auxiliar per calcular si ens apropem a deixar més o menys bicis, si sumem beneficis o costos beneficios o costos
+    def benefici_o_cost(estacion, bicis_a_enviar):
+        diferencia = estacion.num_bicicletas_next + bicis_a_enviar - estacion.demanda
+        if diferencia <= 0:
+            return -diferencia  #Benefici
+        else:
+            return diferencia  #Cost
+
+    # Sol es va a una estació
+    if arribada2 is None:
+        bicis_a_enviar = min(furgoneta.capacitat, self.estacions.lista_estaciones[origen].num_bicicletas_next)
+        self.guanyat += benefici_o_cost(self.estacions.lista_estaciones[arribada1], bicis_a_enviar)
         
-        if arribada2 is None:  # Si sol es va a una estació
-            bicis_a_enviar = min(furgoneta.capacitat, self.estacions.lista_estaciones[origen].num_bicicletas_next)
-            furgoneta.capacitat -= bicis_a_enviar
-            self.estacions.lista_estaciones[origen].num_bicicletas_next -= bicis_a_enviar
-            self.estacions.lista_estaciones[arribada1].num_bicicletas_no_usadas += bicis_a_enviar
-            
-            distancia = self.calcular_distancia_entre_estacions(self.estacions.lista_estaciones[origen], self.estacions.lista_estaciones[arribada1])
-            self.costos += ((bicis_a_enviar + 9) // 10)*distancia
-            
-        else:  # Si n'hi ha dos
-            demanda_arribada1 = self.estacions.lista_estaciones[arribada1].demanda
-            demanda_arribada2 = self.estacions.lista_estaciones[arribada2].demanda
-            
-            bicis_a_enviar_arribada1 = min(furgoneta.capacitat, demanda_arribada1)
-            bicis_a_enviar_arribada2 = min(furgoneta.capacitat - bicicletas_a_enviar_arribada1, demanda_arribada2)
-            
-            furgoneta.capacitat -= (bicis_a_enviar_arribada1 + bicis_a_enviar_arribada2)
-            self.estacions.lista_estaciones[origen].num_bicicletas_next -= (bicis_a_enviar_arribada1 + bicis_a_enviar_arribada2)
-            self.estacions.lista_estaciones[arribada1].num_bicicletas_no_usadas += bicis_a_enviar_arribada1
-            self.estacions.lista_estaciones[arribada2].num_bicicletas_no_usadas += bicis_a_enviar_arribada2
-            
-            distancia1 = self.calcular_distancia_entre_estacions(self.estacions.lista_estaciones[origen], self.estacions.lista_estaciones[arribada1])
-            distancia2 = self.calcular_distancia_entre_estacions(self.estacions.lista_estaciones[arribada1], self.estacions.lista_estaciones[arribada2])
-            self.costos +=  ((bicis_a_enviar_arribada1 + 9) // 10)*distancia1
-            self.costos +=  ((bicis_a_enviar_arribada1 + 9) // 10)*distancia2
+        # Actualitzem la furgoneta i estació
+        furgoneta.capacitat -= bicis_a_enviar
+        furgoneta.destins.append(arribada1)
+        furgoneta.bicis_per_desti[arribada1] = bicis_a_enviar
+        self.estacions.lista_estaciones[origen].num_bicicletas_next -= bicis_a_enviar
+        self.estacions.lista_estaciones[arribada1].num_bicicletas_no_usadas += bicis_a_enviar
         
-        self.visitat[origen] = True
+        # Calcular cost pr distancia
+        distancia = self.calcular_distancia_entre_estacions(self.estacions.lista_estaciones[origen], self.estacions.lista_estaciones[arribada1])
+        self.guanyat -= ((bicis_a_enviar + 9) // 10)*distancia
+
+    else:  # Si es va a dos estacions
+        demanda_arribada1 = self.estacions.lista_estaciones[arribada1].demanda
+        demanda_arribada2 = self.estacions.lista_estaciones[arribada2].demanda
+        
+        bicis_a_enviar_arribada1 = min(furgoneta.capacitat, demanda_arribada1)
+        bicis_a_enviar_arribada2 = min(furgoneta.capacitat - bicis_a_enviar_arribada1, demanda_arribada2)
+        
+        self.guanyat += benefici_o_cost(self.estacions.lista_estaciones[arribada1], bicis_a_enviar_arribada1)
+        self.guanyat += benefici_o_cost(self.estacions.lista_estaciones[arribada2], bicis_a_enviar_arribada2)
+        
+        # Actualitzar furgoneta i estacions
+        furgoneta.capacitat -= (bicis_a_enviar_arribada1 + bicis_a_enviar_arribada2)
+        furgoneta.destins.extend([arribada1, arribada2])
+        furgoneta.bicis_per_desti[arribada1] = bicis_a_enviar_arribada1
+        furgoneta.bicis_per_desti[arribada2] = bicis_a_enviar_arribada2
+        self.estacions.lista_estaciones[origen].num_bicicletas_next -= (bicis_a_enviar_arribada1 + bicis_a_enviar_arribada2)
+        self.estacions.lista_estaciones[arribada1].num_bicicletas_no_usadas += bicis_a_enviar_arribada1
+        self.estacions.lista_estaciones[arribada2].num_bicicletas_no_usadas += bicis_a_enviar_arribada2
+        
+        # Calcular cost per distancia
+        distancia1 = self.calcular_distancia_entre_estacions(self.estacions.lista_estaciones[origen], self.estacions.lista_estaciones[arribada1])
+        distancia2 = self.calcular_distancia_entre_estacions(self.estacions.lista_estaciones[arribada1], self.estacions.lista_estaciones[arribada2])
+        self.guanyat -= ((bicis_a_enviar_arribada1 + 9) // 10)*distancia1
+        self.guanyat -= ((bicis_a_enviar_arribada2 + 9) // 10)*distancia2
+
+    # Asegurarse de actualizar los beneficios y costos
+    self.visitat[origen] = True
+
+        
+        
+
 
 
 class Furgoneta:
-    
-    def __init__(self,origen):
+    def __init__(self, origen):
         self.origen = origen
+        self.destins = []  # Llista de destins
+        self.bicis_per_desti = {}  # Diccionari per saber quantes bicis es deixen a cada destí
         self.capacitat = 30
+
         
     
