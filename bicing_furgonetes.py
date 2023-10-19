8a4eb45db34704eb5507bf4d08f094feb5b58efb
joanbernaus <137653601+joanbernaus@users.noreply.github.com>
2023-10-20 00:51:11 +0200

    Update bicing_furgonetes.py

--- bicing_furgonetes.py
+++ bicing_furgonetes.py
@@ -13,13 +13,15 @@ class Furgonetes(object):
         self.bicis_carregades = bicis_carregades  
         self.bicis_primera = bicis_primera  # Nombre de bicis que es deixen a la primera estació
         self.bicis_segona = bicis_segona  # Nombre de bicis que es deixen a la segona estació
-        self.capacitat = 30 #capacitat màxima de la furgoneta
+        self.capacitat = 30 #Capacitat màxima de la furgoneta
+        self.viatge_fet = False #Marca si ha fet el viatge
 
         if primera_est is None:
             assert bicis_primera == 0  # Si no hi ha primera estació, la quantitat de bicis ha de ser 0
 
         if segona_est is None:
             assert bicis_segona == 0  # Si no hi ha segona estació, la quantitat de bicis ha de ser 0
+
 
 
 
