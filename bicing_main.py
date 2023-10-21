2251f667e82180321977ce6ebceb490b3f44a407
janbennassar <147800871+janbennassar@users.noreply.github.com>
2023-10-21 16:52:04 +0200

    Update bicing_main.py

--- bicing_main.py
+++ bicing_main.py
@@ -1,35 +1,20 @@
 import random
+import time
+
 from aima.search import hill_climbing
 
-from bicing_estacions import Estacio, Estacions
-from bicing_furgonetes import Furgonetes
+from bicing_estacions import Estacion, Estaciones
 
 from bicing_parametres import Parametres
 from bicing_problem import BicingProblem
-from bicing_estat import Estat
+from bicing_estat import genera_estat_inicial_0
 
 
-# Funcions de generació dels diferents estats inicials
 
+params = Parametres(25, 1250, random.randint(2,5000), 5, 30)
+estacions = Estaciones(params.n_estacions, params.n_bicis, params.llavor)
 
-suma_h = 0
-iter = 1
-for i in range(iter):
-    
-    params = Parametres(25, 1250, random.randint(2,5000), 5, 30)
-    estacions = Estacions(params.n_estacions, params.n_bicis, params.llavor)
-
-    estat_inicial = genera_estat_inicial(params, estacions) #Necessari executar per crear l'estat inicial
-
-    #print('Heurística estat inicial: ', estat_inicial.h())
-
-    n = hill_climbing(BicingProblem(estat_inicial))
-
-    #print RUTA de les furgonetes
-
-    suma_h += n.h()
-    #print ('\nHeurística estat inicial', n.h() ) # Valor de l’estat final
-
-
-mitjana = suma_h / iter
-print('Heurística mitjana: ', mitjana)
+estat_inicial = genera_estat_inicial_0(params, estacions)
+h_inicial = estat_inicial.heuristica1()
+n = hill_climbing(BicingProblem(estat_inicial))
+print(f"\nEstat 0: \n  Inicial: {h_inicial} euros Final: {n.heuristica1()} euros")
