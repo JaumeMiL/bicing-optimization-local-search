93b66247060b4e9f516d3e7d44a78ac0814f34f8
janbennassar <147800871+janbennassar@users.noreply.github.com>
2023-10-21 21:30:47 +0200

    Update bicing_main.py

--- bicing_main.py
+++ bicing_main.py
@@ -7,8 +7,7 @@ from bicing_estacions import Estacion, Estaciones
 
 from bicing_parametres import Parametres
 from bicing_problem import BicingProblem
-from bicing_estat import genera_estat_inicial_0
-
+from bicing_estat import genera_estat_inicial_1, genera_estat_inicial_0, genera_estat_inicial_2, Estat
 
 
 params = Parametres(25, 1250, random.randint(2,5000), 5, 30)
@@ -17,4 +16,14 @@ estacions = Estaciones(params.n_estacions, params.n_bicis, params.llavor)
 estat_inicial = genera_estat_inicial_0(params, estacions)
 h_inicial = estat_inicial.heuristica1()
 n = hill_climbing(BicingProblem(estat_inicial))
-print(f"\nEstat 0: \n  Inicial: {h_inicial} euros Final: {n.heuristica1()} euros")
+print(f"\nEstat 0: \n  Inicial: {h_inicial}€ Final: {n.heuristica1()} €")
+
+estat_inicial = genera_estat_inicial_1(params, estacions)
+h_inicial = estat_inicial.heuristica2()
+#n = hill_climbing(BicingProblem(estat_inicial))
+print(f"\nEstat 1: \n  Inicial: {h_inicial}€")
+
+estat_inicial = genera_estat_inicial_2(params, estacions)
+h_inicial = estat_inicial.heuristica2()
+#n = hill_climbing(BicingProblem(estat_inicial))
+print(f"\nEstat 2: \n  Inicial: {h_inicial}€")
